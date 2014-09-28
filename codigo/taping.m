b02105729c2808387d19f234ab08cd3a51ff38fc
partuso18 <artusopablo@gmail.com>
2014-09-28 17:44:46 -0300

    Nuevos codigos

--- codigo/taping.m
+++ codigo/taping.m
@@ -8,20 +8,20 @@ Screen('FillRect',win,white);
 refresh = Screen('GetFlipInterval',win)
 %Synchronize to retrace at start of trial/animation loop:
 vbl = Screen('Flip',win);
-%Loop:Cycle through 300images:ç
+%Loop:Cycle through 300images:
 tic
-for i = 1:10
-	%Showimagesexactly2refreshcyclesapartofeachother:
+for i = 1:30
+	%Show images exactly 2 refresh cycles apart of each other:
 	if mod(i,2) == 0
         Screen('FillRect',win,white);
         Screen('FillOval', win, black, [800, 450, 900, 550]);
 	else
 		Screen('FillRect',win,white);
 	end
-	vbl = Screen('Flip',win,vbl+(2-0.1)*refresh);
-	%Keyboardchecks,whatever...Nextloopiteration.
+	vbl = Screen('Flip',win,vbl+(20-0.1)*refresh);
+	%Keyboard checks, whatever...Next loop iteration.
 end;
-%Endofanimationloop,blankscreen,recordoffsettime:
+%End of animation loop, blankscreen, record offset time:
 toffset = Screen('Flip',win,vbl+(2-0.5)*refresh);
 toc
 Screen('CloseAll');
