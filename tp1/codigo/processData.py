b88a0efe78a27ad71fb7e14e18bcf84fb496521a
Laouen Belloli <lao.facu@gmail.com>
2014-10-27 17:10:53 -0300

    va

--- tp1/codigo/processData.py
+++ tp1/codigo/processData.py
@@ -12,6 +12,12 @@ from math import sqrt
 
 
 
+
+
+
+
+
+
 #Shared auxiliaries
 def getAllResultsFiles():
 	def getFilesFromDir(dirPath):
@@ -22,7 +28,7 @@ def getAllResultsFiles():
 	tp1_path = actual_path.split('/') #['',home,user,....,tp1,codigo] 
 	tp1_path = tp1_path[0:len(tp1_path)-1] #['',home,user,....,tp1]
 
-	results_path = tp1_path + ['resultados2'] #['',home,user,....,tp1,resultados]
+	results_path = tp1_path + ['resultados'] #['',home,user,....,tp1,resultados]
 	results_path = '/'.join(results_path) # /home/user/......../tp1/resultados
 
 	return getFilesFromDir(results_path)
@@ -62,11 +68,77 @@ def saveFigure(figure, file_path, h, t, o):
 
 
 
+def initMatrix():
+
+	files_path = getAllResultsFiles()
+
+	matrix = []
+	for f in files_path:
+		data = loadMetrics(f)
+		data = cleanMetric(data)
+		
+		for p in getAllDataPosibilities():
+			(h,t,o) = p
+			stim_times = data[h][t][o+'S']
+			mes_times = data[h][t][o+'M']
+	
+			person_id = f.split('/')				#['home','user',........'person_id.data']
+			person_id = person_id[len(person_id)-1]			#person_id.data
+			person_id = person_id.split('.')[0]  			#person_id
+
+			diff_times = diffBtwTimes(stim_times,mes_times)
+			for i in range(len(stim_times)):
+				matrix.append([person_id,i,stim_times[i],mes_times[i],diff_times[i],h,t,o])
+
+	return matrix
+
+
 # Processing functions
+def asd():
+	alls='*'
+	matrix = initMatrix()
+	result = []
+	interval = 3
+
+	for i in range(80/interval):
+		datos = filter(lambda x: (x[4] < 0.3 and x[1] in range(i*interval,(i+1)*interval) and x[5] == "LH" and x[6] == "D" and x[7] == "T"),matrix)
+		datos = [e for [_,_,_,_,e,_,_,_] in datos]
+		if len(datos) > 0:
+			prom  = sum(datos)/float(len(datos))
+			result.append(prom)
+	#PLOTTING
+	l1 = range(len(result))
+	plt.plot(l1,result,'ro')
+	figure = plt.gcf()
+	plt.show()
+	#saveFigure(figure,f,h,t,o)
+	plt.gcf().clear()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def diffBtwTimes(stim_times,mes_times):
 	assert(len(stim_times) == len(mes_times))
-	return [stim_times[i] - mes_times[i] for i in range(len(stim_times))]
-
+	result = []
+	for i in range(len(mes_times)):
+		if math.isnan(mes_times[i]):
+			result.append(EPSILON*2)
+		else:
+			result.append(stim_times[i] - mes_times[i]) 
+			
+	return result
 def successesAndFails(ini, fin, mes_times):
 	#The fin index is included in the interval.
 	assert(fin < len(mes_times))
@@ -79,17 +151,19 @@ def successesAndFails(ini, fin, mes_times):
 			successes +=1
 
 	return (successes,fails)
-
-def stdDeviationAndAverage(ini,fin,mes_times):
+def stdDeviationAndAverage(ini,fin,diff_times):
 	#source : http://en.wikipedia.org/wiki/Standard_deviation
-	assert(fin < len(mes_times))
+	assert(fin < len(diff_times))
 	#Cleaning Nans
-	mes_times = [t for t in mes_times if not math.isnan(t)]
+	for i in range(ini,fin+1):
+		if math.isnan(diff_times[i]):
+			diff_times[i] = EPSILON *2
 
-	avg = sum(mes_times) / float(len(mes_times))
+
+	avg = sum(diff_times[ini:fin+1]) / float(len(diff_times[ini:fin+1]))
 
 
- 	std_dev_factors  = mes_times[:]
+ 	std_dev_factors  = diff_times[ini:fin+1]
  	
  	for i in range(len(std_dev_factors)):
  			std_dev_factors[i] -= avg
@@ -103,7 +177,11 @@ def stdDeviationAndAverage(ini,fin,mes_times):
 
 
 
-def plotter():
+
+
+
+
+def plotter_for_each_subject():
 	files_path = getAllResultsFiles()
 
 	for f in files_path:
@@ -116,18 +194,111 @@ def plotter():
 			mes_times = data[h][t][o+'M']
 
 			#FUNCTION TO PLOT
-			result = diffBtwTimes(stim_times,mes_times)
+			###DIFFS###
+			#result = diffBtwTimes(stim_times,mes_times)
+			#
+			#
+			#	
+
+			### AVG and STDVAR
+			result = []
+			diff_times = diffBtwTimes(stim_times,mes_times)
+			for ini in range(len(diff_times)-4):
+				avg,_ = stdDeviationAndAverage(ini,ini+4,diff_times)
+				result.append(avg)
+	
 
 			#PLOTTING
 			l1 = range(len(result))
-			plt.plot(l1,result,'ro')
+			plt.plot(l1,result,'r')
 			figure = plt.gcf()
 			saveFigure(figure,f,h,t,o)
 			plt.gcf().clear()
+def plotter_for_all():
+	files_path = getAllResultsFiles()
 
-			
+	for p in getAllDataPosibilities():
+		result_p = []
+		(h,t,o) = p
+		stim_times = data[h][t][o+'S']
+		mes_times = data[h][t][o+'M']
 
 
+		for f in files_path:
+			data = loadMetrics(f)
+			data = cleanMetric(data)
+
+			### AVG and STDVAR
+			result_subj = []
+			diff_times = diffBtwTimes(stim_times,mes_times)
+			for ini in range(len(diff_times)-4):
+				avg,_ = stdDeviationAndAverage(ini,ini+4,diff_times)
+				result_subj.append(avg)
+
+
+	
+
+	#PLOTTING
+	l1 = range(len(result))
+	plt.plot(l1,result,'r')
+	figure = plt.gcf()
+	saveFigure(figure,f,h,t,o)
+	plt.gcf().clear()
+def histogram_for_all():
+	files_path = getAllResultsFiles()
+
+	for p in getAllDataPosibilities():
+		result_p = []
+		(h,t,o) = p
+
+		diffs = []
+		for f in files_path:
+			data = loadMetrics(f)
+			data = cleanMetric(data)
+			stim_times = data[h][t][o+'S']
+			mes_times = data[h][t][o+'M']
+
+
+			### AVG and STDVAR
+			diff_times = diffBtwTimes(stim_times,mes_times)
+			for i in diff_times:
+				diffs.append(i)
+
+	
+
+	#PLOTTING
+	
+	fig, ax = plt.subplots()
+	print diffs
+
+	p = ax.hist(diffs,facecolor="black")
+	plt.xlim(-0.50, 0.60)
+	plt.show()
+
+	# figure = plt.gcf()
+	# saveFigure(figure,f,h,t,o)
+	# plt.gcf().clear()
+
+
+#Filtering Matrix
+def filterMatrix(cond, matrix):
+	result = []
+	for fila in matrix:
+		if cumple(fila,cond):
+			result.append(fila)
+	return result
+
+def cumple(fila, cond):
+	result = True
+	for i in range(len(fila)):
+		if cond[i] != "*":
+			if fila[i] not in cond[i]:
+				result = False
+	return result
+
+
+
+ 	
 
 
 
